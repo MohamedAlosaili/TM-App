f8baff8fbd45a8e23226da1804e64ff0679e24c6
Mohamed <87425403+MohamedAlosaili@users.noreply.github.com>
2022-07-04 03:47:44 +0300

    Finalize home page design

--- js/app.js
+++ js/app.js
@@ -8,16 +8,31 @@ import {
 // getInfo();
 async function getInfo() {
   try {
-    const req = await fetch(
-      FIND_BY_ID("person", "1372") + "&append_to_response=combined_credits"
-    );
-    console.log(req);
+    const req = await fetch(POPULAR_MOVIES);
     if (!req.ok) throw req.json().errors[0];
     const res = await req.json();
     console.log(res);
+    // getCards(res.results);
   } catch (err) {
     throw err;
   }
+}
+
+const popularMovies = document.querySelector(".card-container");
+
+function getCards(arr) {
+  arr.forEach((movie) => {
+    const figure = document.createElement("figure");
+    figure.classList.add(".card-poster");
+
+    figure.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w300${movie.poster_path}" alt="${
+      movie.original_title
+    }">
+      <figcaption>${movie.original_title ?? movie.title}</figcaption>
+    `;
+    popularMovies.append(figure);
+  });
 }
 const current = new Date();
 const DOB = new Date("1972-06-07");
